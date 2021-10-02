93c450acdf76fd276b1acb5f3e03f984a8d4f5c2
Stuart McColl <7103229+stuartmccoll@users.noreply.github.com>
2021-10-02 13:52:11 +0100

    Added PowerShell profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,19 @@
+# Initialisations
+oh-my-posh --init --shell pwsh --config C:\Users\it\OneDrive\ohmyposh.json | Invoke-Expression
+
+# Module imports
+Import-Module -Name Terminal-Icons
+Import-Module -Name posh-git
+
+# Import local modules
+if (Test-Path ~\OneDrive\Documents\PowerShell\LocalModules)
+{
+
+  $localModules = Get-Item ~\OneDrive\Documents\PowerShell\LocalModules\*
+
+  ForEach ($m in $localModules)
+  {
+    Import-Module $m.FullName
+  }
+
+}
